51785492db00d093765a3c42a54dc3df2ad863a3
Logan Payton <Logan.Payton@javs.com>
2023-10-14 14:21:33 -0500

    Initial Commit

---
+++ ConsoleGraphics.cs
@@ -0,0 +1,93 @@
+using System.Diagnostics;
+using System.Numerics;
+using SkiaSharp;
+
+namespace conimg;
+
+public static class ConsoleGraphics
+{
+    private const char Block = '█';
+    private static readonly ConsoleColor[] _consoleColors = Enum.GetValues<ConsoleColor>();
+
+    private static readonly Vector3[] _consoleColorVectors = {
+        Vector3.Zero,
+        new(0, 0, 0.5451f),
+        new(0, 0.3922f, 0),
+        new(0, 0.5451f, 0.5451f),
+        new(0.5451f, 0, 0),
+        new(0.5451f, 0, 0.5451f),
+        new(0.5451f, 0.5f, 0),
+        new(0.5f, 0.5f, 0.5f),
+        new(0.25f, 0.25f, 0.25f),
+        new(0, 0, 1),
+        new(0, 1, 0),
+        new(0, 1, 1),
+        new(1, 0, 0),
+        new(1, 0, 1),
+        new(1, 1, 0),
+        new(1, 1, 1),
+    };
+
+    public static void Display(SKBitmap bitmap)
+    {
+        var originalColor = Console.ForegroundColor;
+        var iWidth = bitmap.Width;
+        var iHeight = bitmap.Height;
+        var cWidth = Console.WindowWidth;
+        var cHeight = Console.WindowHeight;
+
+        var sw = Stopwatch.StartNew();
+
+        Console.SetCursorPosition(0, 0);
+        for (int cY = 0; cY < cHeight; cY++)
+        {
+            var iY = (int)((double)iHeight * cY / cHeight);
+
+            for (int cX = 0; cX < cWidth; cX++)
+            {
+                var iX = (int)((double)iWidth * cX / cWidth);
+
+                var pixel = bitmap.GetPixel(iX, iY);
+                var color = MapColor(pixel);
+
+                Console.ForegroundColor = color;
+                Console.Write(color is ConsoleColor.Black ? ' ' : Block);
+            }
+
+            Console.WriteLine();
+        }
+
+        sw.Stop();
+        Console.ForegroundColor = originalColor;
+        Console.WriteLine($"Completed in {sw.ElapsedTicks:N0} ticks");
+    }
+
+    private static Vector3 GetVector(SKColor pixel) =>
+        new(pixel.Red / 256f, pixel.Green / 256f, pixel.Blue / 256f);
+
+    private static int GetNearestColorIndex(Vector3 value)
+    {
+        var targetIndex = 0;
+        var targetDistance = Vector3.Distance(_consoleColorVectors[0], value);
+
+        for (int i = 1; i < _consoleColorVectors.Length; i++)
+        {
+            var currentVector = _consoleColorVectors[i];
+            var currentDistance = Vector3.Distance(currentVector, value);
+            if (currentDistance < targetDistance)
+            {
+                targetIndex = i;
+                targetDistance = currentDistance;
+            }
+        }
+
+        return targetIndex;
+    }
+
+    private static ConsoleColor MapColor(SKColor pixel)
+    {
+        var pixelVector = GetVector(pixel);
+        var colorIndex = GetNearestColorIndex(pixelVector);
+        return _consoleColors[colorIndex];
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using conimg;
+using SkiaSharp;
+
+var pngBytes = File.ReadAllBytes("image.png");
+var img = SKBitmap.Decode(pngBytes);
+var imgCopy = img.Copy();
+var con = SKColorFilter.CreateHighContrast(false, SKHighContrastConfigInvertStyle.NoInvert, 0.2f);
+
+using var canvas = new SKCanvas(imgCopy);
+using (var paint = new SKPaint())
+{
+    paint.ColorFilter = con;
+    canvas.DrawBitmap(img, img.Info.Rect, paint: paint);
+}
+
+ConsoleGraphics.Display(imgCopy);
