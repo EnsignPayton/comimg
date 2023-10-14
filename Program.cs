70b8b50aee838f7848695dddcaeedd6b8640940d
Logan Payton <Logan.Payton@javs.com>
2023-10-14 14:36:03 -0500

    Take params

--- Program.cs
+++ Program.cs
@@ -1,16 +1,20 @@
 ﻿using conimg;
 using SkiaSharp;
 
-var pngBytes = File.ReadAllBytes("image.png");
-var img = SKBitmap.Decode(pngBytes);
-var imgCopy = img.Copy();
-var con = SKColorFilter.CreateHighContrast(false, SKHighContrastConfigInvertStyle.NoInvert, 0.2f);
+var path = args.Length >= 1 ? args[0] : "image.png";
+var contrastRatio = args.Length >= 2 && float.TryParse(args[1], out var val) ? val : 0;
 
-using var canvas = new SKCanvas(imgCopy);
+var imageBytes = File.ReadAllBytes(path);
+var imageBitmap = SKBitmap.Decode(imageBytes);
+
+var bitmapCopy = imageBitmap.Copy();
+
+using var canvas = new SKCanvas(bitmapCopy);
 using (var paint = new SKPaint())
 {
-    paint.ColorFilter = con;
-    canvas.DrawBitmap(img, img.Info.Rect, paint: paint);
+    paint.ColorFilter = SKColorFilter.CreateHighContrast(
+        false, SKHighContrastConfigInvertStyle.NoInvert, contrastRatio);
+    canvas.DrawBitmap(imageBitmap, imageBitmap.Info.Rect, paint: paint);
 }
 
-ConsoleGraphics.Display(imgCopy);
+ConsoleGraphics.Display(bitmapCopy);
